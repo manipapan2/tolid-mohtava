f2ec9a7aa715229153423ceb3dec92631d945a92
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-12-02 15:03:02 +0330

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// created by manipapan2
 var screensize = document.getElementById("bodyFake").offsetHeight
 console.log(screensize)
 
@@ -7,11 +8,13 @@ function start(){
     var bb = document.getElementById("bb")
     var created = document.getElementById("created")
     var slider = document.getElementById("slider")
-    goright = document.getElementById("goright")
-    goleft = document.getElementById("goleft")
+    var goright = document.getElementById("goright")
+    var goleft = document.getElementById("goleft")
     charkheshia.classList.add("fadee")
     charkheshia2.classList.add("fadee")
     bb.classList.add("fadee")
+
+    bb.style.transform="scale(0.8)"
 
     setTimeout(() => {
         created.classList.remove("aslit")
@@ -51,4 +54,86 @@ function start(){
         goright.style.opacity="1"
         goleft.style.opacity="1"
     }, "3700");
+
+    setTimeout(() => {
+        goright.style.pointerEvents="painted"
+        goleft.style.pointerEvents="painted"
+    }, "3800");
 }
+
+
+var f1 = document.getElementById("front")
+var f2 = document.getElementById("alr")
+var f3 = document.getElementById("backend")
+var f4 = document.getElementById("json")
+var f5 = document.getElementById("jsonformatter")
+
+
+var f1d = window.getComputedStyle(f1).display;
+var f2d = window.getComputedStyle(f2).display;
+var f3d = window.getComputedStyle(f3).display;
+var f4d = window.getComputedStyle(f4).display;
+var f5d = window.getComputedStyle(f5).display;
+
+
+
+var imagess = [f1,f2,f3,f4,f5]
+if (f1d == "flex"){
+    var goh = 0
+}
+else if (f2d == "flex"){
+    var goh = 1
+}
+else if (f3d == "flex"){
+    var goh = 2
+}
+else if (f4d == "flex"){
+    var goh = 3
+}
+else if (f5d == "flex"){
+    var goh = 4
+}
+
+
+
+function clickright(){
+    imagess[goh].style.display="none"
+    goh += 1
+    imagess[goh].style.display="flex"
+
+    if(goh == 1){
+        goleft.style.display="flex"
+    }
+
+    if(goh == 4){
+        goright.style.display="none"
+    }
+}
+
+
+function clickleft(){
+    imagess[goh].style.display="none"
+    goh -= 1
+    imagess[goh].style.display="flex"
+
+    if(goh == 0){
+        goleft.style.display="none"
+    }
+
+    if(goh == 2){
+        goright.style.display="flex"
+    }
+}
+
+var alri = document.getElementById("alri")
+function alerting(){
+    if(alri.value == ""){
+        alert("The input is empty")
+    }
+    
+    else{
+        alert(alri.value)
+    }
+    
+}
+// created by manipapan2
